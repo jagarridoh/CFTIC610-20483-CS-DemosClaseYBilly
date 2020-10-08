c0cd57c61e37a10f3119939091b0563c7b64481a
Justo Garrido <jgarrido@iies.es>
2020-10-08 18:02:02 +0200

    Inio

---
+++ 20201007-basica/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _20201007_basica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int asf = 0;
+            Console.WriteLine(asf);
+        }
+    }
+}
