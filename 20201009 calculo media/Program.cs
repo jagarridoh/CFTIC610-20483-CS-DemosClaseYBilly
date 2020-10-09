29eb101b2983b55180f91ef50386acaed5d203c1
Justo Garrido <jgarrido@iies.es>
2020-10-09 19:38:59 +0200

    Ejemplos 9 oct

---
+++ 20201009 calculo media/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _20201009_calculo_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] num = new double[10];
+            double suma = 0;
+            int cont = default;
+            string message = "The average of ";
+            for (cont=0; cont<10; cont++) {
+                string entrada = "";
+                double? valor = null;
+                do {
+                    Console.Write("Enter the number #{cont} (or empty to end): ");
+                    entrada = Console.ReadLine();
+                    if (entrada=="") break;
+                    try {
+                        valor = Convert.ToDouble(entrada);
+                    } catch(e) {}
+                } while (valor == null);
+                if (entrada!="") {
+                    num[cont] = (double) valor;
+                    message += entrada + " ";
+                    suma += (double) valor;
+                }
+            }
+            Console.WriteLine($"{message} is: {suma/cont}");
+        }
+    }
+}
